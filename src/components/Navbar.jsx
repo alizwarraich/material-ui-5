03d17575e02d4167325b0f79f4f0005ad6fc7703
Ali Zulqarnain <warraichleopard@gmail.com>
2022-08-24 22:28:43 +0500

    Completed Menu, Sidebar and Cards

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,76 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography, styled } from '@mui/material'
-import { Pets } from '@mui/icons-material';
+import { AppBar, Toolbar, Box, Typography, styled, InputBase, Badge, Avatar, Menu, MenuItem } from '@mui/material'
+import { Mail, Notifications, Pets } from '@mui/icons-material';
 
 const StyledToolbar = styled(Toolbar)({
     display: 'flex',
     justifyContent: 'space-between',
 });
+const Search = styled("div")(({ theme }) => ({
+    backgroundColor: "white",
+    padding: '0 10px',
+    borderRadius: theme.shape.borderRadius,
+    width: '40%'
+}));
+const Icons = styled(Box)(({ theme }) => ({
+    display: 'none',
+    gap: 20,
+    alignItems: 'center',
+    [theme.breakpoints.up('sm')]: {
+        display: 'flex'
+    }
+}));
+const UserBox = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    gap: 10,
+    alignItems: 'center',
+    [theme.breakpoints.up('sm')]: {
+        display: 'none'
+    }
+}));
+
 
 const Navbar = () => {
+    const [open, setOpen] = React.useState(false)
     return (
         <AppBar position='sticky'>
             <StyledToolbar>
                 <Typography variant="h6" sx={{ display: { xs: 'none', sm: 'block' } }}>Dev</Typography>
                 <Pets sx={{ display: { xs: 'block', sm: 'none' } }} />
+                <Search><InputBase placeholder="search..." /></Search>
+                <Icons>
+                    <Badge badgeContent={4} color="error">
+                        <Mail />
+                    </Badge>
+                    <Badge badgeContent={2} color="error">
+                        <Notifications />
+                    </Badge>
+                    <Avatar onClick={() => setOpen(true)} sx={{ width: 30, height: 30 }} />
+                </Icons>
+                <UserBox>
+                    <Avatar onClick={() => setOpen(true)} sx={{ width: 30, height: 30 }} />
+                    <Typography variant="span">John</Typography>
+                </UserBox>
             </StyledToolbar>
-        </AppBar>
+            <Menu
+                id="demo-positioned-menu"
+                aria-labelledby="demo-positioned-button"
+                open={open}
+                onClose={() => setOpen(false)}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+            >
+                <MenuItem>Profile</MenuItem>
+                <MenuItem>My account</MenuItem>
+                <MenuItem>Logout</MenuItem>
+            </Menu>
+        </AppBar >
     )
 }
 
--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import Box from '@mui/material/Box'
+import { Avatar, AvatarGroup, Typography } from '@mui/material'
 
 const Rightbar = () => {
     return (
         <Box
-            bgcolor="skyblue"
             flex={2}
             p={2}
-            sx={{ display: { xs: 'none', sm: 'block' } }}>
-            Rightbar
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+        >
+            <Box position="fixed">
+                <Typography variant="h6" fontWeight={100}>Friends Online</Typography>
+                <AvatarGroup max={7}>
+                    <Avatar alt="Mr A" src="https://material-ui.com/static/images/avatar/1.jpg" />
+                    <Avatar alt="Mr B" src="https://material-ui.com/static/images/avatar/2.jpg" />
+                    <Avatar alt="Mr C" src="https://material-ui.com/static/images/avatar/3.jpg" />
+                    <Avatar alt="Mr D" src="https://material-ui.com/static/images/avatar/4.jpg" />
+                    <Avatar alt="Mr E" src="https://material-ui.com/static/images/avatar/5.jpg" />
+                    <Avatar alt="Mr F" src="https://material-ui.com/static/images/avatar/6.jpg" />
+                    <Avatar alt="Mr G" src="https://material-ui.com/static/images/avatar/7.jpg" />
+                    <Avatar alt="Mr H" src="https://material-ui.com/static/images/avatar/8.jpg" />
+                    <Avatar alt="Mr I" src="https://material-ui.com/static/images/avatar/9.jpg" />
+                </AvatarGroup>
+            </Box>
         </Box>
     )
 }
